e2ed4b85b8b57399a02f211d1c6b4f7944fcfc57
Chris MacNaughton <chris@centaurisolutions.nl>
2018-09-05 12:13:33 +0200

    add header response

--- src/lib.rs
+++ src/lib.rs
@@ -2,8 +2,41 @@
 extern crate cpython;
 #[macro_use]
 extern crate lando;
+extern crate http;
+extern crate serde_json;
 
-use lando::{Response, RequestExt};
+use lando::{LambdaContext, Response, Request, RequestExt};
+
+const HEADER_BLACKLIST: [&'static str; 12] = [
+    "x-amz-cf-id",
+    "cloudfront-viewer-country",
+    "x-forwarded-port",
+    "cloudfront-is-smarttv-viewer",
+    "x-forwarded-for",
+    "cloudfront-is-tablet-viewer",
+    "cloudfront-forwarded-proto",
+    "x-amzn-trace-id",
+    "cloudfront-is-mobile-viewer",
+    "cloudfront-is-desktop-viewer",
+    "via",
+    "x-forwarded-proto",
+];
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_filters_headers() {
+        let mut request = http::Request::builder();
+        request.header("host", "test");
+        request.header("x-forwarded-port", "blah");
+
+        let headers = filtered_headers(&request.body("".into()).unwrap()).unwrap();
+        assert!(headers.get("x-forwarded-port").is_none());
+        assert_eq!(headers.get("host").unwrap(), "test");
+    }
+}
 
 gateway!(
     "hello" => |request, _| {
@@ -24,4 +57,24 @@ gateway!(
 
         Ok(response)
     },
+    "headers" => headers
 );
+
+fn headers(request: Request, _context: LambdaContext) -> Result<Response<String>, Box<std::error::Error>> {
+        let mut response = Response::builder();
+        let response = response.body(filtered_headers(&request)?.to_string())?;
+
+        Ok(response)
+}
+
+fn filtered_headers(request: &Request) -> Result<serde_json::Value, Box<std::error::Error>> {
+    let mut headers = serde_json::map::Map::new();
+    for header in request.headers() {
+        if !HEADER_BLACKLIST.contains(&header.0.as_str()) {
+            headers.insert(
+                header.0.as_str().to_string(), serde_json::Value::String(header.1.to_str()?.to_string())
+            );
+        }
+    }
+    Ok(serde_json::Value::Object(headers))
+}
