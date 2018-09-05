fbafe7fc55aeb27e9c6454421a918717fd4cba51
Chris MacNaughton <chris@centaurisolutions.nl>
2018-09-05 13:24:53 +0200

    Add all request

--- src/lib.rs
+++ src/lib.rs
@@ -2,26 +2,88 @@
 extern crate cpython;
 #[macro_use]
 extern crate lando;
+extern crate http;
+#[macro_use]
+extern crate serde_json;
 
-use lando::{Response, RequestExt};
+use lando::{LambdaContext, Response, Request, RequestExt};
+
+const HEADER_BLACKLIST: [&'static str; 12] = [
+    "x-amz-cf-id",
+    "cloudfront-viewer-country",
+    "x-forwarded-port",
+    "cloudfront-is-smarttv-viewer",
+    "x-forwarded-for",
+    "cloudfront-is-tablet-viewer",
+    "cloudfront-forwarded-proto",
+    "x-amzn-trace-id",
+    "cloudfront-is-mobile-viewer",
+    "cloudfront-is-desktop-viewer",
+    "via",
+    "x-forwarded-proto",
+];
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_filters_headers() {
+        let mut request = http::Request::builder();
+        request.header("host", "test");
+        request.header("x-forwarded-port", "blah");
+
+        let headers = filtered_headers(&request.body("".into()).unwrap()).unwrap();
+        assert!(headers.get("x-forwarded-port").is_none());
+        assert_eq!(headers.get("host").unwrap(), "test");
+    }
+}
 
 gateway!(
-    "hello" => |request, _| {
-        println!("{:?}", request);
-        Ok(lando::Response::new(format!(
-            "hello {}",
-            request
-                .path_parameters()
-                .get("name")
-                .cloned()
-                .unwrap_or_else(|| "stranger".to_owned())
-        )))
-    },
-    "ip" => |request, _context| {
+    "ip" => ip_req,
+    "headers" => headers_req,
+    "all" => all,
+);
+
+fn all(request: Request, _context: LambdaContext) -> Result<Response<String>, Box<std::error::Error>> {
+    let body = json!({
+        "ip": ip(&request),
+        "headers": filtered_headers(&request)?,
+    });
+
+    let mut response = Response::builder();
+    let response = response.body(body.to_string())?;
+
+    Ok(response)
+}
+
+fn ip_req(request: Request, _context: LambdaContext) -> Result<Response<String>, Box<std::error::Error>> {
         let mut response = Response::builder();
         response.header("content-type", "text/plain");
-        let response = response.body(request.request_context().identity.source_ip)?;
+        let response = response.body(ip(&request))?;
 
         Ok(response)
-    },
-);
+}
+
+fn headers_req(request: Request, _context: LambdaContext) -> Result<Response<String>, Box<std::error::Error>> {
+        let mut response = Response::builder();
+        let response = response.body(filtered_headers(&request)?.to_string())?;
+
+        Ok(response)
+}
+
+fn filtered_headers(request: &Request) -> Result<serde_json::Value, Box<std::error::Error>> {
+    let mut headers = serde_json::map::Map::new();
+    for header in request.headers() {
+        if !HEADER_BLACKLIST.contains(&header.0.as_str()) {
+            headers.insert(
+                header.0.as_str().to_string(), serde_json::Value::String(header.1.to_str()?.to_string())
+            );
+        }
+    }
+    Ok(serde_json::Value::Object(headers))
+}
+
+fn ip(request: &Request) -> String {
+    request.request_context().identity.source_ip
+}
